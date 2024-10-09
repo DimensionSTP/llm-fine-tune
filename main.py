264341862bcfef36b3fe50d1e84575c1317a0828
DimensionSTP <ddang8jh@gmail.com>
2024-10-09 23:54:55 +0900

    refactor: main config name

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from src.pipelines.pipeline import train, test, predict, tune
 
 @hydra.main(
     config_path="configs/",
-    config_name="huggingface.yaml",
+    config_name="cpt.yaml",
 )
 def main(
     config: DictConfig,
