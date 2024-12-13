f184528c72763c7ad71c5a67d74fa2104e48fd3a
DimensionSTP <ddang8jh@gmail.com>
2024-12-13 17:43:22 +0900

    refactor: default model_type

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -2,7 +2,7 @@
 
 path="src/preprocessing"
 upload_user="meta-llama"
-model_type="Meta-Llama-3.1-8B-Instruct"
+model_type="Llama-3.1-8B-Instruct"
 
 python $path/merge_tokenizer.py upload_user=$upload_user model_type=$model_type
 python $path/merge_model.py upload_user=$upload_user model_type=$model_type
