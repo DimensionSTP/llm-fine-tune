4e32b1e9bf85805d64336225c65cb90fb53a23e7
DimensionSTP <ddang8jh@gmail.com>
2025-03-18 17:33:54 +0900

    add: postprocesses __init__

---
+++ src/postprocessing/__init__.py
@@ -0,0 +1,11 @@
+from prepare_upload_all import prepare_upload as prepare_upload_all
+from prepare_upload import prepare_upload
+from upload_all_to_hf_hub import upload_to_hf_hub as upload_all_to_hf_hub
+from upload_to_hf_hub import upload_to_hf_hub
+
+__all__ = [
+    "prepare_upload_all",
+    "prepare_upload",
+    "upload_all_to_hf_hub",
+    "upload_to_hf_hub",
+]
