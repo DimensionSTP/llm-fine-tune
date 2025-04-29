4e5fba81cf5b625e41b2586cfe1860852ab5bbff
DimensionSTP <ddang8jh@gmail.com>
2025-04-29 13:48:37 +0900

    fix: __init__ import

--- src/postprocessing/__init__.py
+++ src/postprocessing/__init__.py
@@ -1,8 +1,8 @@
-from convert_zero_ckpt import convert_zero_ckpt
-from prepare_upload_all import prepare_upload as prepare_upload_all
-from prepare_upload import prepare_upload
-from upload_all_to_hf_hub import upload_to_hf_hub as upload_all_to_hf_hub
-from upload_to_hf_hub import upload_to_hf_hub
+from .convert_zero_ckpt import convert_zero_ckpt
+from .prepare_upload_all import prepare_upload as prepare_upload_all
+from .prepare_upload import prepare_upload
+from .upload_all_to_hf_hub import upload_to_hf_hub as upload_all_to_hf_hub
+from .upload_to_hf_hub import upload_to_hf_hub
 
 __all__ = [
     "convert_zero_ckpt",
